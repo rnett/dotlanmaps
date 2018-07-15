f63795d7ffc012337cd3c5d1abdad14925aaac6a
Ryan Nett <rnett@calpoly.edu>
2018-07-14 22:53:18 -0700

    gradle publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ import java.net.URL
 plugins {
     java
     kotlin("jvm") version "1.2.51"
+    `maven-publish`
+    maven
 }
 
 group = "com.rnett.ligraph.eve"
@@ -12,7 +14,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven ("https://dl.bintray.com/kotlin/exposed")
-    maven("https://jitpack.io")
     maven("https://dl.bintray.com/kotlin/ktor")
     jcenter()
 }
@@ -28,4 +29,29 @@ configure<JavaPluginConvention> {
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    from(java.sourceSets["main"].allSource)
+}
+
+artifacts.add("archives", sourcesJar)
+
+publishing {
+    publications {
+        create("default", MavenPublication::class.java) {
+            from(components["java"])
+            artifact(sourcesJar)
+        }
+        create("mavenJava", MavenPublication::class.java) {
+            from(components["java"])
+            artifact(sourcesJar)
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repository")
+        }
+    }
 }
